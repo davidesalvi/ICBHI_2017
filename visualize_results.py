9198e7b0e3570bf696a856bcfa14cb35b9339d1a
Davide Salvi <davide.salvi@polimi.it>
2025-01-16 17:19:52 +0100

    visualize_data code

--- visualize_results.py
+++ visualize_results.py
@@ -3,7 +3,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import roc_curve, auc, confusion_matrix
 
 
@@ -15,6 +15,26 @@ def compute_eer_auc(labels, pred):
     eer = fpr[np.nanargmin(np.absolute(fnr - fpr))]
 
     return eer, rocauc
+
+
+def plot_roc_curve(labels, pred, legend=None):
+
+    fpr, tpr, thres = roc_curve(labels, pred)
+    rocauc = auc(fpr, tpr)
+
+    if legend:
+        plt.plot(fpr, tpr, lw=3, label=legend + ' - AUC = %0.2f' % rocauc)
+    else:
+        plt.plot(fpr, tpr, lw=3, label='AUC = %0.2f' % rocauc)
+    plt.plot([0, 1], [0, 1], color='black', lw=3, linestyle='--')
+    plt.xlim([-0.02, 1.0])
+    plt.ylim([0.0, 1.03])
+    plt.xlabel('False Positive Rate', fontsize=18)
+    plt.ylabel('True Positive Rate', fontsize=18)
+    plt.legend(loc="lower right", fontsize=15)
+    plt.xticks(fontsize=15)
+    plt.yticks(fontsize=15)
+    plt.grid(True)
 
 
 def plot_confusion_matrix(y_true, y_pred, label_dict, normalize=False):
@@ -34,9 +54,9 @@ def plot_confusion_matrix(y_true, y_pred, label_dict, normalize=False):
     cm_df = pd.DataFrame(cm, index=labels, columns=labels)
     cm_df.replace(0, np.nan, inplace=True)
 
-    fsize = 25
+    fsize = 20
 
-    plt.figure()
+    plt.figure(figsize=(6,6))
     plt.imshow(cm_df, interpolation='nearest', cmap=plt.cm.Blues, aspect='auto')
 
     tick_marks = np.arange(len(labels))
@@ -59,71 +79,52 @@ def visualize_results():
 
     result_dir = './results'
 
-    classification_types = ['multi', 'binary']
     model_types = ['RESNET', 'LCNN']
     feature_types = ['MelSpec', 'LogSpec', 'MFCC', 'LFCC']
-    win_lens = ['5.0', '10.0']
+    win_lens = [5.0, 10.0]
 
-    for classification in classification_types:
-        for model in model_types:
+    classification_type = 'binary'
+
+    for model in model_types:
+        for win_len in win_lens:
+
+            plt.figure(figsize=(8,8))
+            plt.title(f"{model} - {win_len}sec")
+
             for feature in feature_types:
-                for win_len in win_lens:
 
-                    if classification == 'binary':
-                        label_dict = {'Healthy': 0, 'Unhealthy': 1}
-                    else:
-                        label_dict = {'Healthy': 0, 'COPD': 1, 'URTI': 2, 'Asthma': 3, 'LRTI': 4, 'Bronchiectasis': 5,
-                                      'Pneumonia': 6, 'Bronchiolitis': 7}
-
-                    model_name = f"{model}_{classification}_{feature}_{win_len}sec.txt"
-                    results = pd.read_csv(os.path.join(result_dir, model_name), sep=' ', header=None)
-                    results.columns = ['filename', 'pred', 'label']
-
-                    if classification == 'binary':
-                        bal_acc = balanced_accuracy_score(results['label'], results['pred'])
-                        print(f"{model}_{classification}_{feature}_{win_len}sec - Bal. Acc.: {bal_acc}")
-
-                        eer, rocauc = compute_eer_auc(results['label'], results['pred'])
-                        print(f"EER: {eer:.2f} ROC-AUC: {rocauc:.2f}")
-
-                    else:
-                        bal_acc = balanced_accuracy_score(results['label'], results['pred'])
-                        print(f"{model}_{classification}_{feature}_{win_len}sec - Bal. Acc.: {bal_acc}")
-
-                    plot_confusion_matrix(results['label'], results['pred'], normalize=True, label_dict=label_dict)
-
-
-                    y_true = results[0].values
-                    y_pred = results[1].values
-                    plot_confusion_matrix(y_true, y_pred, normalize=True)
-                    plt.savefig(f"{model_name.split('.')[0]}.png")
-                    plt.close()
-
-
-    result_dir = '/nas/home/dsalvi/ICBHI_2017/results'
-    feature_sets = ['MelSpec', 'LogSpec', 'MFCC', 'LFCC']
-    classification_types = ['multi', 'binary']
-    durations = ['5.0', '10.0']
-
-    for feature in feature_sets:
-        for classification in classification_types:
-            for duration in durations:
-
-
-                result_name = f"RESNET_{classification}_{feature}_{duration}sec.txt"
-                results = pd.read_csv(os.path.join(result_dir, result_name), sep=' ', header=None)
+                model_name = f"{model}_{classification_type}_{feature}_{win_len}sec.txt"
+                results = pd.read_csv(os.path.join(result_dir, model_name), sep=' ', header=None)
                 results.columns = ['filename', 'pred', 'label']
 
-                print(f"{feature}, {classification}, {duration} - Bal.Acc.: {balanced_accuracy_score(results['label'], results['pred'])*100:.2f}")
-        print()
+                plot_roc_curve(results['label'], results['pred'], legend=feature)
 
-                # plot_confusion_matrix(results['label'], results['pred'], normalize=True, label_dict=label_dict)
-                #
-                # y_true = results[0].values
-                # y_pred = results[1].values
-                # plot_confusion_matrix(y_true, y_pred, normalize=True)
-                # plt.savefig(f"{model_name.split('.')[0]}.png")
-                # plt.close()
+                label_dict = {'Healthy': 0, 'Unhealthy': 1}
+                plot_confusion_matrix(results['label'], np.round(results['pred']), normalize=True, label_dict=label_dict)
+
+                eer, rocauc = compute_eer_auc(results['label'], results['pred'])
+                print(f"{model}_{classification_type}_{feature}_{win_len}sec - EER: {eer:.2f} - ROC-AUC: {rocauc:.2f}")
+
+            plt.show()
+
+
+    classification_type = 'multi'
+
+    for model in model_types:
+        for feature in feature_types:
+            for win_len in win_lens:
+
+                label_dict = {'Healthy': 0, 'COPD': 1, 'URTI': 2, 'Asthma': 3, 'LRTI': 4, 'Bronchiectasis': 5,
+                              'Pneumonia': 6, 'Bronchiolitis': 7}
+
+                model_name = f"{model}_{classification_type}_{feature}_{win_len}sec.txt"
+                results = pd.read_csv(os.path.join(result_dir, model_name), sep=' ', header=None)
+                results.columns = ['filename', 'pred', 'label']
+
+                bal_acc = accuracy_score(results['label'], results['pred'])
+                print(f"{model}_{classification_type}_{feature}_{win_len}sec - Bal. Acc.: {bal_acc:.2f}")
+
+                plot_confusion_matrix(results['label'], results['pred'], normalize=True, label_dict=label_dict)
 
 
 if __name__ == '__main__':
